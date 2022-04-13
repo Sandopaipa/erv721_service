6fa2e8aee077d0967a8449329add334da5260571
Sandopaipa <kirill.kuli.01@gmail.com>
2022-04-13 03:29:36 +0300

    Initial commit

---
+++ erv721_service/erv721_service/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for erv721_service project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'erv721_service.settings')
+
+application = get_asgi_application()
